95fe1bc7f8f9ad531c9483c1efd07de65bef4642
Dennis Lui <hackout@vip.qq.com>
2022-06-07 12:31:23 +0800

    更新

--- TencentKYC.php
+++ TencentKYC.php
@@ -1,278 +1,131 @@
 <?php
 
-class TencentKYC
-{
-	
+class TencentKYC {
+
 	/**
 	 * Uri
 	 * @var string
 	 */
-	protected $url = 'https://miniprogram-kyc.tencentcloudapi.com/api/oauth2';
+	protected $url = 'https://faceid.tencentcloudapi.com';
 
 	/**
-	 * WebBank APP ID
+	 * Rule ID
 	 * @var string
 	 */
-	protected $webankID;
-	/**
-	 * APPID
-	 * @var string
-	 */
-	protected $app_id;
-
-	/**
-	 * Secret
-	 * @var string
-	 */
+	protected $rule_id;
+	protected $appid;
 	protected $secret;
-
-	/**
-	 * Access Token
-	 * @var string
-	 */
-	protected $access_token;
-
-	/**
-	 * Ticket
-	 * @var array
-	 */
-	protected $tickets = [
-		'sign' => [],
-		'notice' => []
-	];
-	/**
-	 * openData
-	 * @var array
-	 */
-	protected $openData = [];
-
-	protected $defaultUrl = 'miniprogram-kyc.tencentcloudapi.com';
 
 	/**
 	 * Create a new tencent-kyc instance.
 	 *
 	 * @param string $webank
-	 * @param string $app_id
 	 * @param string $secret
 	 * @return void
 	 */
-	public function __construct($webank,$app_id, $secret) {
-		$this->webankID = $webank;
-		$this->app_id = $app_id;
-		$this->access_token = $secret;
+	public function __construct($rule_id, $appid, $secret) {
+		$this->rule_id = $rule_id;
+		$this->appid = $appid;
+		$this->secret = $secret;
 	}
 
-	protected function getUrl($str)
-	{
-		return $this->url .$str;
-	}
-
-	protected function error($msg)
-	{
-		return ['code' => 0,'msg' => $msg];
-	}
-
-	protected function success($data)
-	{
-		return ['code' => 1,'msg' => 'success','data' => $data];
-	}
-
-	/**
-	 * Get Access Token
-	 * @return array
-	 */
-	protected function getAccessToken()
-	{
-		$url = $this->getUrl('/access_token');
-		$param = [
-			'app_id' => $this->app_id,
-			'secret' => $this->secret,
-			'grant_type' => 'client_credential',
-			'version' => '1.0.0'
+	public function openKYC($name, $idcard) {
+		$data = [
+			'RuleId' => $this->rule_id,
+			'IdCard' => $idcard,
+			'Name' => $name,
+			'RedirectUrl' => WP . '?weixin-phone-index-bindface',
 		];
-
-		$result = $this->sendRequest($url,$param);
-		if(!is_array($result)) return $this->error($result);
-		if($result['code'] != '0') return $this->error($result['msg']);
-		$this->access_token = $result['access_token'];
-		return $this->success([
-			'transactionTime' => $result['transactionTime'],
-			'access_token' => $result['access_token'],
-			'expire_time' => $result['expire_time'],
-			'expire_in' => $result['expire_in']
-		]);
+		$result = $this->sendRequest($this->url, $data);
+		return isset($result['Response']) && isset($result['Response']['BizToken']) ? ['code' => 1, 'url' => $result['Response']['Url'], 'faceid' => $result['Response']['BizToken']] : ['code' => 0, 'msg' => '获取授权链接失败'];
 	}
 
-	protected function getSignTicket()
-	{
-		$url = $this->getUrl('/api_ticket');
-		$param = [
-			'app_id' => $this->app_id,
-			'access_token' => $this->access_token,
-			'type' => 'SIGN',
-			'version' => '1.0.0'
-		];
+	protected function makeSign($data, $date) {
+		$secretId = $this->appid;
+		$secretKey = $this->secret;
+		$host = "faceid.tencentcloudapi.com";
+		$service = "faceid";
+		$version = "2017-03-12";
+		$action = "DetectAuth";
+		$region = "";
+		$timestamp = $date;
+		$algorithm = "TC3-HMAC-SHA256";
 
-		$result = $this->sendRequest($url,$param);
-		if(!is_array($result)) return $this->error($result);
-		if($result['code'] != '0') return $this->error($result['msg']);
-		$this->tickets['sign'] = $result['tickets'];
-		return $this->success([
-			'transactionTime' => $result['transactionTime'],
-			'ticket' => $result['tickets']['value'],
-			'expire_time' => $result['tickets']['expire_time'],
-			'expire_in' => $result['tickets']['expire_in']
-		]);
+		$httpRequestMethod = "POST";
+		$canonicalUri = "/";
+		$canonicalQueryString = "";
+		$canonicalHeaders = "content-type:application/json; charset=utf-8\n" . "host:" . $host . "\n";
+		$signedHeaders = "content-type;host";
+		$payload = $data;
+		$hashedRequestPayload = hash("SHA256", $payload);
+		$canonicalRequest = $httpRequestMethod . "\n"
+			. $canonicalUri . "\n"
+			. $canonicalQueryString . "\n"
+			. $canonicalHeaders . "\n"
+			. $signedHeaders . "\n"
+			. $hashedRequestPayload;
+
+		$date = gmdate("Y-m-d", $timestamp);
+		$credentialScope = $date . "/" . $service . "/tc3_request";
+		$hashedCanonicalRequest = hash("SHA256", $canonicalRequest);
+		$stringToSign = $algorithm . "\n"
+			. $timestamp . "\n"
+			. $credentialScope . "\n"
+			. $hashedCanonicalRequest;
+
+		$secretDate = hash_hmac("SHA256", $date, "TC3" . $secretKey, true);
+		$secretService = hash_hmac("SHA256", $service, $secretDate, true);
+		$secretSigning = hash_hmac("SHA256", "tc3_request", $secretService, true);
+		$signature = hash_hmac("SHA256", $stringToSign, $secretSigning);
+
+		$authorization = $algorithm
+			. " Credential=" . $secretId . "/" . $credentialScope
+			. ", SignedHeaders=content-type;host, Signature=" . $signature;
+		return $authorization;
 	}
-
-	protected function getNoticeTicket()
-	{
-		$url = $this->getUrl('/api_ticket');
-		$param = [
-			'app_id' => $this->app_id,
-			'access_token' => $this->access_token,
-			'type' => 'NONCE',
-			'version' => '1.0.0'
-		];
-
-		$result = $this->sendRequest($url,$param);
-		if(!is_array($result)) return $this->error($result);
-		if($result['code'] != '0') return $this->error($result['msg']);
-		$this->tickets['notice'] = $result['tickets'];
-		return $this->success([
-			'transactionTime' => $result['transactionTime'],
-			'ticket' => $result['tickets']['value'],
-			'expire_time' => $result['tickets']['expire_time'],
-			'expire_in' => $result['tickets']['expire_in']
-		]);
-	}
-
-	public function verifyLogin($data )
-	{
-		$param = array_merge([
-			'code' => null,
-			'orderNo' => null,
-			'h5faceId' => null,
-			'newSignature' => null,
-			'liveRate' => null,
-		],$data);
-
-		$sign = [
-			'app_id' => $this->app_id,
-			'orderNo' => $param['orderNo'],
-			'code' => $param['code'],
-			'ticket' => $this->tickets['sign']['value']
-		];
-		$signature = $this->makeSign($sign);
-
-		if($signature == $param['newSignature'])
-		{
-			return $param;
-		}
-		return ;
-	}
-
-
-	protected function postUser($order_no,$userId,$name,$idcard)
-	{
-		$url = 'https://miniprogram-kyc.tencentcloudapi.com/api/server/h5/geth5faceid?orderNo='.$order_no;
-		$param = [
-			'webankAppId' => $this->webankID,
-			'orderNo' => $orderNo,
-			'name' => $name,
-			'idNo' => $idcard,
-			'userId' => $userId,
-			'version' => '1.0.0',
-			'ticket' => $this->tickets['sign']['value']
-		];
-
-		$param['sign'] = $this->makeSign($param);
-
-		$result = $this->sendRequest($url,$param);
-		if(!is_array($result)) return $this->error($result);
-		if($result['code'] != '0') return $this->error($result['msg']);
-		$this->openData = $result['result'];
-		return $this->success([
-			'transactionTime' => $result['transactionTime'],
-			'bizSeqNo' => $result['result']['bizSeqNo'],
-			'orderNo' => $result['result']['orderNo'],
-			'h5faceId' => $result['result']['h5faceId'],
-			'optimalDomain' => $result['result']['optimalDomain'],
-		]);
-	}
-
-
-	public function openKYC()
-	{
-		return [];
-	}
-
-	protected function openUrl()
-	{
-		$url = 'https://'.($this->openData['optimalDomain'] ? $this->openData['optimalDomain'] : $this->defaultUrl).'/api/web/login';
-		$param = [
-			'webankAppId' => $webankID,
-			'orderNo' => $order,
-			'userId' => $user_id,
-			'version' => '1.0.0',
-			'h5faceId' => $this->openData['h5faceId'],
-			'nonce' => md5(time()),
-			'ticket' => $this->tickets['notice']['value']
-		];
-		$param['sign'] = $this->makeSign($param);
-		$param = [
-			'webankAppId' => $param['webankAppId'],
-			'version' => $param['version'],
-			'nonce' => $param['nonce'],
-			'orderNo' => $param['orderNo'],
-			'h5faceId' => $param['h5faceId'],
-			'url' => 'url',
-			'resultType' => 1,
-			'userId' => $param['userId'],
-			'sign' => $param['sign'],
-			'from' => 'browser',
-			'redirectType' => 1,
-		];
-		return $url.'?'.http_build_query($param);
-	}
-
-
-	protected function makeSign($key,$data)
-	{
-		$str = '';
-		foreach($data as $r)
-		{
-			$str .= $r;
-		}
-		$str .= $key;
-		return sha1($str);
-	}
-
 
 	/**
 	 * Send a request
-	 * @param  string $url  
-	 * @param  array  $data 
-	 * @return array|string 
+	 * @param  string $url
+	 * @param  array  $data
+	 * @return array|string
 	 */
-	protected function sendRequest($url,$data = [])
-	{
-		if(!$url) return ;
-		if($data)
-		{
-			$data = http_build_query($data);
+	protected function sendRequest($url, $data = []) {
+		if (!$url) {
+			return;
 		}
-		$opts = [
-			'http' => [
-				'method' => 'POST',
-				'header' => "Content-type:application/x-www-form-urlencoded Content-Length:".strlen ($data),
-				'content' => $data
-			]
-		];
-		$context =  stream_context_create($opts);
-		$result = file_get_contents($url,false,$context);
+		$data = json_encode($data, 256);
+		$time = time();
+		$curl = curl_init();
+		$headers['X-TC-Action'] = "DetectAuth";
+		$headers['X-TC-Timestamp'] = $time;
+		$headers['X-TC-Version'] = "2018-03-01";
+		$headers['X-TC-Language'] = "zh-CN";
+		$headers['Content-Type'] = "application/json; charset=utf-8";
+		$headers['Authorization'] = $this->makeSign($data, $time);
+		$header = [];
+		foreach ($headers as $key => $rs) {
+			$header[] = $key . ':' . $rs;
+		}
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => $data,
+			CURLOPT_HTTPHEADER => $header,
+		));
+
+		$result = curl_exec($curl);
+
+		curl_close($curl);
+
 		try {
-			return json_decode($result,true);
+			return json_decode($result, true);
 		} catch (\Exception $e) {
 			return $result;
 		}
